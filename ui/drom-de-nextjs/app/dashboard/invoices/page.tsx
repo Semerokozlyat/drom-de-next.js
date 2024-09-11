3972bd449d9331f151d392cf389cb3d57995729e
evgeniy.pomortsev <evgeniy.pomortsev@acronis.com>
2024-09-11 23:09:59 +0200

    Next.js tutor, chapter 16: adding page metadata

--- ui/drom-de-nextjs/app/dashboard/invoices/page.tsx
+++ ui/drom-de-nextjs/app/dashboard/invoices/page.tsx
@@ -6,6 +6,11 @@ import { lusitana } from '@/app/ui/fonts';
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 import { Suspense } from 'react';
 import { fetchInvoicesPages } from '@/app/lib/data';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+    title: 'Invoices',   // this "Invoices" string goes to the Metadata template, defined in "layout.tsx"
+};
 
 export default async function Page({ searchParams }: { searchParams?: {
     query?: string;
